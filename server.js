da44a888fa419cbdb3bc6bd4c046f6ab8d3a2347
Sagar0101 <sagarrajani20@gmail.com>
2022-06-17 01:54:55 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,50 @@
+require('dotenv').config()
+const express = require("express")
+const app = express()
+const jwt  = require('jsonwebtoken')
+
+app.use(express.json())
+
+const posts = [
+    {
+        username: "sagar",
+        title: "post 1"
+    },
+    {
+        username: "ags",
+        title: "psot 2"
+    }
+]
+
+
+
+app.get('/posts', authenticateToken, (req,res) => {
+    res.json(posts.filter(post => post.username === req.user.name))
+})
+
+app.post('/login', (req,res) => {
+    //authentication
+    const username = req.body.username
+    const user = {name: username}
+    
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({accessToken: accessToken})
+
+
+})
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if(token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) =>{
+        if(err) return res.sendStatus(403)
+        // console.log(req.user)
+        req.user = user
+        // console.log(req.user)
+        next()
+    })
+}
+
+app.listen(3000)
